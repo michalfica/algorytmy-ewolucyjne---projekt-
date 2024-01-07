09cc957fbd15ae8f492df1a6ec7857380fcc6bf4
Michal <michalfica125@gmail.com>
2024-01-07 12:57:33 +0100

    kształt losowej plamki poprawiony

--- projekt/individual.py
+++ projekt/individual.py
@@ -6,19 +6,23 @@ class Splash:
     """
     R             - promień plamy 
     """
-    R = 2 
+    R = 5 
     MAX_RANK  = 10 
+
+    RED_COL = np.array([255, 0, 0], dtype=np.int64)
+    GREEN_COL = np.array([0, 255, 0], dtype=np.int64)
+    BLUE_COL = np.array([0, 0, 25], dtype=np.int64)
 
     def __init__(self, color, rank):
         self.color = color
         self.rank  = rank 
 
     def random_splash(self, max_rank):
-        self.color = np.array([np.random.randint(0,255) for i in range(3)])
+        self.color = np.array([np.random.randint(0,255) for i in range(3)], dtype=np.int64)
         self.rank = np.random.randint(0, max_rank)
     
     def red_splash(self, max_rank= MAX_RANK):
-        self.color = np.array([0, 0, 255])
+        self.color = Splash.RED_COL
         self.rank = max_rank
 
     def __str__(self):
@@ -32,7 +36,7 @@ class Individual:
     LENGTH, WIDTH - parametry obrazka
     N             - liczba plam  
     """
-    LENGTH, WIDTH = 10, 10   
+    LENGTH, WIDTH = 200, 200   
     N             = 100
 
     """
@@ -52,23 +56,24 @@ class Individual:
             return ((pixel[0] < 0 or Individual.LENGTH <= pixel[0]) or
                     (pixel[1] < 0 or Individual.WIDTH <= pixel[1]))
          
-        def outside_of_splash(pixel, splash_x, splash_y):
-            return (pixel[0]-x)**2 + (pixel[1]-y)**2 <= Splash.R**2
+        def outside_of_splash(pixel, x, y):
+            return (pixel[0]-x)**2 + (pixel[1]-y)**2 > Splash.R**2
         
-        pixels_array = np.zeros((Individual.WIDTH, Individual.LENGTH, 3))
+        pixels_array = np.zeros((Individual.WIDTH, Individual.LENGTH, 3), dtype=np.int64)
         pixels_array_ranks = np.zeros((Individual.WIDTH, Individual.LENGTH, 1))
 
         splash_index = 0 
         for x,y in self.splash_layout:
             splash = self.splash_parameters[splash_index]
+            splash_index += 1
 
-            for t in range(-Individual.R,Individual.R+1):
-                for s in range(-Individual.R,Individual.R+1):
+            for t in range(-Splash.R,Splash.R+1):
+                for s in range(-Splash.R,Splash.R+1):
 
                     pixel = (int(x+t), int(y+s))
                     if outside_of_frame(pixel) or outside_of_splash(pixel, x, y):
                         continue
-                    
+
                     if pixels_array_ranks[pixel[0]][pixel[1]] < splash.rank:
                         pixels_array[pixel[0]][pixel[1]] = splash.color
                         pixels_array_ranks[pixel[0]][pixel[1]] = splash.rank
