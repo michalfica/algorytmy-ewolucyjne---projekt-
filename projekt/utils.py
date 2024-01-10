ee43216f0fe594b6d58594df7460de0d7c7c40b3
Michal <michalfica125@gmail.com>
2024-01-10 12:09:08 +0100

    zakodowana monalisa, szwakuje generowanie obrazków o takim samym roziarze co mona

--- projekt/utils.py
+++ projekt/utils.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 from skimage import io
 
 import individual 
@@ -11,16 +10,12 @@ import numpy as np
 
 class Utils:
 
-
-    #  TE RZECZY POWINNY WYLĄDOWAĆ JEDNAK W  klasie PROBLEM !! 
-    def each_pixel_of_picture(picture_name):
-        image = io.imread(picture_name)
-        each_pixel = image.ravel()
-        return each_pixel
-
     def __init__(self, picture_name):
-        self.objective_picture = Utils.each_pixel_of_picture(picture_name)  
-    
+        self.objective_picture = io.imread(picture_name)
+        l, w, t = self.objective_picture.shape
+        self.length, self.width = l, w 
+
+
     def objective_function(self, individual):
         result = 0 
         # przechodze po kazdym pixelu obrazka 
