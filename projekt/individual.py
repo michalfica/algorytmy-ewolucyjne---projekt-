d4dbe7dc78290e973c13b3fa4651f55bdcb48f49
Michal <michalfica125@gmail.com>
2024-01-10 11:44:51 +0100

    restrukturyzacja, osobnu plik na Splash

--- projekt/individual.py
+++ projekt/individual.py
@@ -2,35 +2,13 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 
-class Splash:
-    """
-    R             - promień plamy 
-    """
-    R = 5 
-    MAX_RANK  = 10 
+import splash
+from imp import reload 
+reload(splash)
 
-    RED_COL = np.array([255, 0, 0], dtype=np.int64)
-    GREEN_COL = np.array([0, 255, 0], dtype=np.int64)
-    BLUE_COL = np.array([0, 0, 25], dtype=np.int64)
+from splash import Splash
 
-    def __init__(self, color, rank):
-        self.color = color
-        self.rank  = rank 
 
-    def random_splash(self, max_rank):
-        self.color = np.array([np.random.randint(0,255) for i in range(3)], dtype=np.int64)
-        self.rank = np.random.randint(0, max_rank)
-    
-    def red_splash(self, max_rank= MAX_RANK):
-        self.color = Splash.RED_COL
-        self.rank = max_rank
-
-    def __str__(self):
-        return f'<{self.color[0]}, {self.color[1]}, {self.color[2]}>'
-
-    def __repr__(self):
-        return f'[{self.color[0]}, {self.color[1]}, {self.color[2]}]'
-    
 class Individual:
     """
     LENGTH, WIDTH - parametry obrazka
@@ -40,12 +18,18 @@ class Individual:
     N             = 100
 
     """
-    splash_parameters - tablica z parametrami kolejnych plam (kolorem rangą)
-    splash_layout     - tablica z rozmieszczeniem kolejnych plam (para współrzędnych)
+    splash_parameters - tablica z parametrami kolejnych plam (kolorem, rangą, położeniem)
     """
-    def __init__(self, splash_parameters, splash_layout):
+    def __init__(self, splash_parameters=None):
         self.splash_parameters = splash_parameters
-        self.splash_layout = splash_layout
+        self.objective_value = None 
+
+    def generate_random_inidividual(self):
+        splash_list = [Splash() for i in range(Individual.N)]
+        for splash in splash_list:
+            splash.random_splash(Splash.MAX_RANK, Individual.LENGTH, Individual.WIDTH)
+
+        self.splash_parameters = splash_list
 
     """
     zwraca tablice z wartością koloru w kazdym pixelu obrazka 
@@ -56,22 +40,20 @@ class Individual:
             return ((pixel[0] < 0 or Individual.LENGTH <= pixel[0]) or
                     (pixel[1] < 0 or Individual.WIDTH <= pixel[1]))
          
-        def outside_of_splash(pixel, x, y):
-            return (pixel[0]-x)**2 + (pixel[1]-y)**2 > Splash.R**2
+        def outside_of_splash(pixel, x, y, r):
+            return (pixel[0]-x)**2 + (pixel[1]-y)**2 > r**2
         
         pixels_array = np.zeros((Individual.WIDTH, Individual.LENGTH, 3), dtype=np.int64)
         pixels_array_ranks = np.zeros((Individual.WIDTH, Individual.LENGTH, 1))
+ 
+        for splash in self.splash_parameters:
+            x, y = splash.x, splash.y 
 
-        splash_index = 0 
-        for x,y in self.splash_layout:
-            splash = self.splash_parameters[splash_index]
-            splash_index += 1
-
-            for t in range(-Splash.R,Splash.R+1):
-                for s in range(-Splash.R,Splash.R+1):
+            for t in range(-splash.r,splash.r+1):
+                for s in range(-splash.r,splash.r+1):
 
                     pixel = (int(x+t), int(y+s))
-                    if outside_of_frame(pixel) or outside_of_splash(pixel, x, y):
+                    if outside_of_frame(pixel) or outside_of_splash(pixel, x, y, splash.r):
                         continue
 
                     if pixels_array_ranks[pixel[0]][pixel[1]] < splash.rank:
---
+++ projekt/splash.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class Splash:
+    DEFAULT_R = 15 
+    MAX_RANK  = 10 
+
+    BLACK = np.array([0, 0, 0], dtype=np.int64)
+
+    def __init__(self, color=BLACK, rank=MAX_RANK, x=0, y=0, r=DEFAULT_R):
+        self.color = color
+        self.rank  = rank 
+        self.r = r 
+        self.x, self.y = x, y 
+
+    def random_splash(self, max_rank, length, width):
+        self.color = np.array([np.random.randint(0,255) for i in range(3)], dtype=np.int64)
+        self.rank = np.random.randint(0, max_rank)
+        self.r = np.random.randint(1, Splash.DEFAULT_R+1)
+        self.x = np.random.randint(0, length)
+        self.y = np.random.randint(0, width)
+
+    def __str__(self):
+        return f'<{self.color[0]}, {self.color[1]}, {self.color[2]}>'
+
+    def __repr__(self):
+        return f'[{self.color[0]}, {self.color[1]}, {self.color[2]}]'
+    
